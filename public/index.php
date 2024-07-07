9ec487f05eafa0b13641d4009c66f752e4f4d167
Alexandr <airmanjke57@gmail.com>
2024-07-07 16:19:21 +0200

    2nd step

--- public/index.php
+++ public/index.php
@@ -2,14 +2,21 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello World!");
-    return $response;
+//$app->get('/', function (Request $request, Response $response, $args) {
+    //$response->getBody()->write("Hello World!");
+    //return $response;
+//});
+
+$app->get('/', function ($request, $response, $args) {
+    $renderer = new PhpRenderer(__DIR__ . '/../templates');
+    return $renderer->render($response, "index.phtml", $args);
 });
+
 
 $app->run();
