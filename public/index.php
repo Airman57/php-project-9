3e71b256d2da2b3bf55da9b44e9b9f5695b7c009
Alexandr <airmanjke57@gmail.com>
2024-08-06 09:33:00 +0200

    linter fix

--- public/index.php
+++ public/index.php
@@ -121,7 +121,10 @@ $app->get('/urls/{id}', function ($request, $response, $args) {
                         ->withHeader('Content-Type', 'text/html')
                         ->write('Url not found (:');
     }
-    $checkedUrl = $this->get('pdo')->query("SELECT * FROM url_checks WHERE url_id = $urlId ORDER BY created_at DESC")->fetchAll();
+    $checkedUrl = $this->get('pdo')->query("SELECT * FROM url_checks 
+                                            WHERE url_id = $urlId 
+                                            ORDER BY created_at DESC")
+                                    ->fetchAll();
     $messages = $this->get('flash')->getMessages();
     $params = ['url' => $url,
                'flash' => $messages,
